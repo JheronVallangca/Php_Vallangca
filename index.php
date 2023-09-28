6269982eb838d8b16aa2d9213333889c7002a86a
Jheron <jdv1104@student.uc-bcf.edu.ph>
2023-09-29 01:47:12 +0800

    first commit

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>PHP</title>
+    </head>
+    <body>
+        <?php
+            echo "Hello World";
+            echo "Legit ba";
+            echo "huy";
+            echo "hala gumana na live server";
+        ?>
+    </body>
+</html>
